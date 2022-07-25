5a57d669ebf063dde2a75a325f29f374982f1161
Blake Sartin <blake.sartin8@gmail.com>
2022-07-24 17:41:08 -0700

    set up basic topbar styling

--- food_service_app/src/App.js
+++ food_service_app/src/App.js
@@ -1,15 +1,19 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import Topbar from "./components/Topbar";
+import Drinks from "./components/Drinks";
+import Food from "./components/Food";
+import Bills from "./components/Bills";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Drinks/>
-      <Food/>
-      <Tables/>
-      <Bills/>
-      <Checkout/>
+      <Drinks />
+      <Food />
+      <Bills />
+      <Checkout />
     </div>
   );
 }
---
+++ food_service_app/src/components/Topbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Topbar.scss"
+
+export default function Topbar() {
+  return <nav className="nav">
+    <a href="/" className="site-title">F&D</a>
+    <ul>
+      <li>
+        <a href="/food">Food</a>
+        </li>
+        <li>
+        <a href="/drinks">Drinks</a>
+        </li>
+        <li>
+        <a href="/tables">Tables</a>
+        </li>
+        <li>
+        <a href="/bills">Bills</a>
+        </li>
+        <li>
+        <a href="/checkout">Checkout</a>
+        </li>
+      
+    </ul>
+    </nav>
+}
