2e98d9b97e1696dde1aa7be7c3f410cb2d099ac6
Blake Sartin <blake.sartin8@gmail.com>
2022-07-25 21:16:51 -0700

    add card to drinks

--- food_service_app/src/components/Drinks.js
+++ food_service_app/src/components/Drinks.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "./Drinks.scss"
 
 
 export default function Drinks() {
   return <div className="container">
-    <a className="title">Drinks</a>
+    <a>Drinks</a>
+    <div className="card">
+      <img></img>
+      <h1 className="title">Title</h1>
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
     </div>
 }
