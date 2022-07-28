ecd763b436d4dd5e5dc488a42212e36ed09221b1
Blake Sartin <blake.sartin8@gmail.com>
2022-07-27 19:26:50 -0700

    add card styling for dirnk component

--- food_service_app/src/components/Drinks.js
+++ food_service_app/src/components/Drinks.js
@@ -4,12 +4,69 @@ import "./Drinks.scss"
 
 export default function Drinks() {
   return <div className="container">
-    <a>Drinks</a>
     <div className="card">
-      <img></img>
-      <h1 className="title">Title</h1>
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
       <h2 className="description">Description</h2>
       <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
+      </div>
+      <div className="card">
+      <img src="https://images.pexels.com/photos/2795026/pexels-photo-2795026.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      <div className="text">
+      <h2 className="description">Description</h2>
+      <h3 className="price">Price</h3>
+      </div>
+      <h3 className="title">Title</h3>
       </div>
     </div>
 }
