f8d2e0b9e6ef0efc03377a869c93ca26508c2f8e
Blake Sartin <blake.sartin8@gmail.com>
2022-07-28 19:05:07 -0700

    add tables, seats, and styling for both

--- food_service_app/src/components/Tables.js
+++ food_service_app/src/components/Tables.js
@@ -1,8 +1,54 @@
 import React from "react";
+import "./Tables.scss"
+import {Chair, TableBar} from "@mui/icons-material"
+
 
 
 export default function Tables() {
-  return <div className="container">
-    <a className="title">Tables</a>
+  return <div className="table-container">
+    <div className="table">
+      <h2>1 <TableBar/></h2>
+      <button className="seat">1 <Chair  fontSize="small"/></button>
+      <button className="seat">2 <Chair  fontSize="small"/></button>
+      <button className="seat">3 <Chair  fontSize="small"/></button>
+      <button className="seat">4 <Chair  fontSize="small"/></button>
+      <button className="seat">5 <Chair  fontSize="small"/></button>
+      <button className="seat">6 <Chair  fontSize="small"/></button>
+      <button className="seat">7 <Chair  fontSize="small"/></button>
+      <button className="seat">8 <Chair  fontSize="small"/></button>
+      </div>
+      <div className="table">
+      <h2>2 <TableBar/></h2>
+      <button className="seat">1 <Chair  fontSize="small"/></button>
+      <button className="seat">2 <Chair  fontSize="small"/></button>
+      <button className="seat">3 <Chair  fontSize="small"/></button>
+      <button className="seat">4 <Chair  fontSize="small"/></button>
+      <button className="seat">5 <Chair  fontSize="small"/></button>
+      <button className="seat">6 <Chair  fontSize="small"/></button>
+      <button className="seat">7 <Chair  fontSize="small"/></button>
+      <button className="seat">8 <Chair  fontSize="small"/></button>
+      </div>
+      <div className="table">
+      <h2>3 <TableBar/></h2>
+      <button className="seat">1 <Chair  fontSize="small"/></button>
+      <button className="seat">2 <Chair  fontSize="small"/></button>
+      <button className="seat">3 <Chair  fontSize="small"/></button>
+      <button className="seat">4 <Chair  fontSize="small"/></button>
+      <button className="seat">5 <Chair  fontSize="small"/></button>
+      <button className="seat">6 <Chair  fontSize="small"/></button>
+      <button className="seat">7 <Chair  fontSize="small"/></button>
+      <button className="seat">8 <Chair  fontSize="small"/></button>
+      </div>
+      <div className="table">
+      <h2>4 <TableBar/></h2>
+      <button className="seat">1 <Chair  fontSize="small"/></button>
+      <button className="seat">2 <Chair  fontSize="small"/></button>
+      <button className="seat">3 <Chair  fontSize="small"/></button>
+      <button className="seat">4 <Chair  fontSize="small"/></button>
+      <button className="seat">5 <Chair  fontSize="small"/></button>
+      <button className="seat">6 <Chair  fontSize="small"/></button>
+      <button className="seat">7 <Chair  fontSize="small"/></button>
+      <button className="seat">8 <Chair  fontSize="small"/></button>
+      </div>
     </div>
 }
