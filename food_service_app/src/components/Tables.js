df44c992513b928a84cb3d60d8c3405537f03c35
Blake Sartin <blake.sartin8@gmail.com>
2022-07-28 19:35:53 -0700

    added tabel styling

--- food_service_app/src/components/Tables.js
+++ food_service_app/src/components/Tables.js
@@ -2,53 +2,42 @@ import React from "react";
 import "./Tables.scss"
 import {Chair, TableBar} from "@mui/icons-material"
 
-
+let data = [
+  {
+    table: "1",
+    seats: ["1","2","3","4","5","6","7","8"],
+  },
+  {
+    table: "2",
+    seats: ["1","2","3","4"],
+  },
+  {
+    table: "3",
+    seats: ["1","2","3","4","5","6"],
+  },
+  {
+    table: "4",
+    seats: ["1","2","3"],
+  },
+  {
+    table: "5",
+    seats: ["1","2"],
+  },
+  {
+    table: "6",
+    seats: ["1","2","3","4","5","6","7"],
+  },
+]
 
 export default function Tables() {
   return <div className="table-container">
-    <div className="table">
-      <h2>1 <TableBar/></h2>
-      <button className="seat">1 <Chair  fontSize="small"/></button>
-      <button className="seat">2 <Chair  fontSize="small"/></button>
-      <button className="seat">3 <Chair  fontSize="small"/></button>
-      <button className="seat">4 <Chair  fontSize="small"/></button>
-      <button className="seat">5 <Chair  fontSize="small"/></button>
-      <button className="seat">6 <Chair  fontSize="small"/></button>
-      <button className="seat">7 <Chair  fontSize="small"/></button>
-      <button className="seat">8 <Chair  fontSize="small"/></button>
-      </div>
+    {data.map((d) => (
       <div className="table">
-      <h2>2 <TableBar/></h2>
-      <button className="seat">1 <Chair  fontSize="small"/></button>
-      <button className="seat">2 <Chair  fontSize="small"/></button>
-      <button className="seat">3 <Chair  fontSize="small"/></button>
-      <button className="seat">4 <Chair  fontSize="small"/></button>
-      <button className="seat">5 <Chair  fontSize="small"/></button>
-      <button className="seat">6 <Chair  fontSize="small"/></button>
-      <button className="seat">7 <Chair  fontSize="small"/></button>
-      <button className="seat">8 <Chair  fontSize="small"/></button>
+      <h2>{d.table} <TableBar/></h2>
+      {d.seats.map((seat) => (
+        <button className="seat">{seat} <Chair  fontSize="small"/></button>
+      ))}
       </div>
-      <div className="table">
-      <h2>3 <TableBar/></h2>
-      <button className="seat">1 <Chair  fontSize="small"/></button>
-      <button className="seat">2 <Chair  fontSize="small"/></button>
-      <button className="seat">3 <Chair  fontSize="small"/></button>
-      <button className="seat">4 <Chair  fontSize="small"/></button>
-      <button className="seat">5 <Chair  fontSize="small"/></button>
-      <button className="seat">6 <Chair  fontSize="small"/></button>
-      <button className="seat">7 <Chair  fontSize="small"/></button>
-      <button className="seat">8 <Chair  fontSize="small"/></button>
-      </div>
-      <div className="table">
-      <h2>4 <TableBar/></h2>
-      <button className="seat">1 <Chair  fontSize="small"/></button>
-      <button className="seat">2 <Chair  fontSize="small"/></button>
-      <button className="seat">3 <Chair  fontSize="small"/></button>
-      <button className="seat">4 <Chair  fontSize="small"/></button>
-      <button className="seat">5 <Chair  fontSize="small"/></button>
-      <button className="seat">6 <Chair  fontSize="small"/></button>
-      <button className="seat">7 <Chair  fontSize="small"/></button>
-      <button className="seat">8 <Chair  fontSize="small"/></button>
-      </div>
+    ))}
     </div>
 }
