d8756ddc16c5c2b5c1d02e1882f332a5dcc3451c
Blake Sartin <blake.sartin8@gmail.com>
2022-08-17 13:32:00 -0700

    change seat to an array of objects containing numbers and ordered food, then change map to show ordered food and styled ordered food

--- food_service_app/src/components/Tables.js
+++ food_service_app/src/components/Tables.js
@@ -1,33 +1,71 @@
 import React, { useState } from "react";
-import "./Tables.scss"
-import {Chair, TableBar} from "@mui/icons-material"
+import "./Tables.scss";
+import { Chair, TableBar } from "@mui/icons-material";
 
 let data = [
   {
     table: "1",
-    seats: ["1","2","3","4","5","6","7","8"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "3", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "4", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "5", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "6", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "7", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "8", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
   {
     table: "2",
-    seats: ["1","2","3","4"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "3", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "4", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
   {
     table: "3",
-    seats: ["1","2","3","4","5","6"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "3", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "4", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "5", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "6", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
   {
     table: "4",
-    seats: ["1","2","3"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "3", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "4", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "5", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
   {
     table: "5",
-    seats: ["1","2"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
   {
     table: "6",
-    seats: ["1","2","3","4","5","6","7"],
+    seats: [
+      { number: "1", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "2", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "3", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "4", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "5", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "6", ordered: ["Cheese Burger, Fries, Coke"] },
+      { number: "7", ordered: ["Cheese Burger, Fries, Coke"] },
+    ],
   },
-]
+];
 
 export default function Tables() {
   const [isActive, setActive] = useState("false");
@@ -35,18 +73,27 @@ export default function Tables() {
   const handleToggle = () => {
     setActive(!isActive);
   };
-  
-  return <div className={isActive ? "table_container" : "expanded_table_container"}>
 
-    <button onClick={handleToggle}>BUTTON</button>
-    {data.map((d) => (
-      <div className={isActive ? "table" : "expanded_table"}>
-      <button className={isActive ? "table-button" : "expanded_table_button"} >{d.table} <TableBar/></button>
-      {d.seats.map((seat) => (
-        <button className={isActive ? "seat" : "expanded_seat"}>{seat} <Chair  fontSize="small"/></button>
+  return (
+    <div className={isActive ? "table_container" : "expanded_table_container"}>
+      {data.map((d) => (
+        <div className={isActive ? "table" : "expanded_table"}>
+          <button
+            className={isActive ? "table-button" : "expanded_table_button"}
+            onClick={handleToggle}
+          >
+            {d.table} <TableBar />
+          </button>
+          {d.seats.map((seat) => (
+            <>
+            <button className={isActive ? "seat" : "expanded_seat"}>
+              {seat.number} <Chair fontSize="small" />
+            </button>
+            <h1 className={isActive ? null : "ordered"}>{seat.ordered}</h1>
+            </>
+          ))}
+        </div>
       ))}
-      </div>
-    ))}
     </div>
-    
+  );
 }
