d27c9e2948d5bab671e0872001768e8f458537ad
Blake Sartin <blake.sartin8@gmail.com>
2022-10-16 15:27:20 -0700

    add drink and food button to tables, that navigates to food and drink components and will eventually add drinks and food to hightlighted seat

--- food_service_app/src/components/Table.js
+++ food_service_app/src/components/Table.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./Tables.scss";
-import { Chair, TableBar } from "@mui/icons-material";
+import { Chair, TableBar, LocalBar, LunchDining } from "@mui/icons-material";
 import { Add, Remove } from "@mui/icons-material";
-import { display } from "@mui/system";
 
 export default function Table({ table }) {
   const [isActive, setActive] = useState("false");
@@ -14,7 +13,7 @@ export default function Table({ table }) {
   return (
     <div className={isActive ? "table_container" : "expanded_table_container"}>
       <div className={isActive ? "table" : "expanded_table"}>
-        <div className={!isActive ? "button_container": null}>
+        <div className={!isActive ? "button_container" : null}>
           <button
             className={isActive ? "table-button" : "expanded_table_button"}
             onClick={handleToggle}
@@ -30,6 +29,19 @@ export default function Table({ table }) {
             </button>
           </div>
         </div>
+        <div className={!isActive ? "food_and_drink_button_container" : null}>
+          <a href="/food">
+          <button className={isActive ? "food" : "expanded_food"}>
+            <LunchDining fontSize="small" />
+          </button>
+          </a>
+          <a href="/drinks">
+          <button className={isActive ? "drink" : "expanded_drink"}>
+            <LocalBar fontSize="small" />
+          </button>
+          </a>
+        </div>
+
         {table.seats.map((seat) => (
           <>
             <button className={isActive ? "seat" : "expanded_seat"}>
